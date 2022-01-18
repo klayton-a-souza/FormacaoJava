43e8777ed492d7a1a4a29f5e051ec818d590da78
Klayton <klayton.al.souza@gmail.com>
2022-01-18 10:38:34 -0300

    06.Finally e try resources
    Essa parte e referente a parte 03, referente ao módulo 06

--- java-pilha/src/Conexao.java
+++ java-pilha/src/Conexao.java
@@ -1,16 +1,19 @@
-public class Conexao {
+public class Conexao implements AutoCloseable {
 
     public Conexao() {
         System.out.println("Abrindo conexão");
+        throw new IllegalStateException();
     }
 
     public void leDados() {
         System.out.println("Recebendo dados");
-        // throw new IlegalStateException();
+        throw new IllegalStateException();        
     }
 
-    public void fechar() {
+    @Override
+    public void close() {
         System.out.println("Fechando conexão");
+
     }
 
 }
--- java-pilha/src/TesteConexao.java
+++ java-pilha/src/TesteConexao.java
@@ -2,15 +2,31 @@ public class TesteConexao {
 
     public static void main(String[] args) {
 
-        Conexao conexao = null;
-        try {
-            conexao = new Conexao();
+        // So e possivel usar o try com no minimo um catch, ou com um try finally ou com
+        // os 3 como no exemplo abaixo, mais nunca somente o try
+
+        try(Conexao conexao = new Conexao()){
             conexao.leDados();
-        } catch (IllegalStateException exception) {
+        } catch (IllegalAccessError exeception){
             System.out.println("Deu erro na conexão");
-        } finally {
-            conexao.fechar();
         }
+        
+        /*
+         * Conexao conexao = null;
+         * 
+         * try {
+         * conexao = new Conexao();
+         * conexao.leDados();
+         * } catch (IllegalStateException exception) {
+         * System.out.println("Deu erro na conexão");
+         * } finally {
+         * System.out.println("finally");
+         * if(conexao != null){
+         * conexao.close();;
+         * }
+         * 
+         * }
+         */
 
     }
 
