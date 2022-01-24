f5de75e12522f9694bf3eb624858359e1e77e88b
Klayton <klayton.al.souza@gmail.com>
2022-01-24 10:11:10 -0300

    04. O pacote java.lang
    Curso:
    Java e java.lang: Programe com a classe Object e String
    
    Conhecendo java.lang

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -4,7 +4,10 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 
+//br.com.bytebank.banco.teste.TesteContas
 public class TesteContas {
+	
+	//java.lang.String
 	public static void main(String[] args) throws SaldoInsuficienteException {
 
 		
