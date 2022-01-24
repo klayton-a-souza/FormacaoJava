f53c43a6fe2d481a3dffcc4df4f4815604ed7fc4
Klayton <klayton.al.souza@gmail.com>
2022-01-24 10:48:11 -0300

    04. O pacote java.lang
    Curso:
    Java e java.lang: Programe com a classe Object e String
    
    Praticando alguns métodos utilizado em um String

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,72 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Alura";// object literal
+
+		// String outroNome = new String ("Alura");
+
+		/*
+		 * nome.replace("A", "a");
+		 * nome.toLowerCase();
+		 * 
+		 * System.out.println(nome);
+		 * 
+		 * // String e imutavel, uma vez criado ela nao pode ser alterada.
+		 * 
+		 * String outra = nome.replace("Al", "aL");
+		 * String outraMinuscula = nome.toLowerCase();
+		 * String outraMaiusculo = nome.toUpperCase();
+		 * System.out.println("Trocando o Al pelo aL, utilizando o replace: " + outra);
+		 * System.out.println("Todas letras minusculas: " + outraMinuscula);
+		 * System.out.println("Todas letras maisculas: " + outraMaiusculo);
+		 */
+
+		/*
+		 * char c = nome.charAt(2); // Ele apresenta qual valor esta nesse index
+		 * System.out.println(c);
+		 */
+
+		/*
+		 * int posicao = nome.indexOf("ur");//Ele apresenta em qual index esta esse
+		 * valor
+		 * System.out.println(posicao);
+		 */
+
+		/*
+		 * String sub = nome.substring(1); //Criando um subString passando o index
+		 * inicial como parametro
+		 * System.out.println(sub);
+		 */
+
+		/*
+		 * System.out.println(nome.length()); //Para saber quantos caracteres possui
+		 * esse String
+		 * 
+		 * for (int i = 0; i < nome.length(); i++) {
+		 * System.out.println(nome.charAt(i));
+		 * }
+		 */
+
+		/*
+		 * String vazio = " Klayton      ";
+		 * String testeTrim = vazio.trim(); //Esse metodo remove todos os espaços vazios
+		 * " "
+		 * System.out.println(vazio.isEmpty()); //Esse metodo faz uma verificação se o
+		 * String esta vazio ou não, retornando true ou false
+		 * System.out.println(vazio);
+		 * System.out.println(testeTrim);
+		 */
+
+		/*
+		 * String possui = "Klayton";
+		 * System.out.println(possui.contains("yt"));
+		 * // O metodo conteins() executa uma verificação procurando o string que você
+		 * passou como parametro dentro do String indicado
+		 */
+
+	}
+
+}
