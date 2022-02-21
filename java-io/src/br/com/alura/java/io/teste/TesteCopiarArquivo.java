c86395f657cc85ea54e04d1c21244c7b70a11cda
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-02-21 11:23:14 -0300

    02. Escrita com Java.IO
    Java e java.io: Streams, Reader e Writers

--- java-io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ java-io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -12,24 +12,27 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 
-public class Teste {
+public class TesteCopiarArquivo {
 
     public static void main(String[] args) throws IOException {
 
-        InputStream fis = new FileInputStream("lorem.txt");
+        InputStream fis = System.in; //new FileInputStream("lorem.txt");
+        //System.in = Entrada pelo teclado.
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
-        OutputStream fos = new FileOutputStream("lorem3.txt");
+        OutputStream fos = System.out; //new FileOutputStream("lorem3.txt");    
+        //System.out = Saida no console.     
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
+        //Dessa maneira o BufferedWriter fica guardando os valores que foram passado atravez do InputStream ate sair do laço
 
         String linha = br.readLine();
 
-        while (linha != null) {
-            System.out.println(linha);
+        while (linha != null && !linha.isEmpty()) {            
             bw.write(linha);
             bw.newLine();
+            bw.flush(); // Esse metodo e para resolver o problema que o BuffereWriter fica guardando os valores, com ele toda vez que um nova linha seja inserida logo em seguida ela vai aparecer no console.
             linha = br.readLine();
 
         }
