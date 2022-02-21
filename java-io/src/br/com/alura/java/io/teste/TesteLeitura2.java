fef489aef858abbed77d27aecd04ccc30059471b
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-02-21 14:03:28 -0300

    04. Usando java.util.Scanner
    Java e java.io: Streams, Reader e Writers
    
    03. Parseando arquivo

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -12,6 +13,31 @@ public class TesteLeitura2 {
         while (scanner.hasNextLine()) {
             String linha = scanner.nextLine();
             System.out.println(linha);
+
+            Scanner linhaScanner = new Scanner(linha);
+            linhaScanner.useLocale(Locale.US); 
+            //Essa linha fala para o compilador qual regra deve usar, no brasil os 
+            //valores são separados por virgulas, diferente dos estados unidos que
+            //separado por ponto. Logo escrevendo essa linha de codigo indicamos para
+            //o compilador qual regra deve usar.
+            linhaScanner.useDelimiter(",");
+
+            String primeiroValor = linhaScanner.next();
+            int segundoValor = linhaScanner.nextInt();
+            int terceiroValor = linhaScanner.nextInt();
+            String quartoValor = linhaScanner.next();
+            double quintoValor = linhaScanner.nextDouble();
+
+            System.out.println(primeiroValor + segundoValor + terceiroValor + quartoValor + quintoValor);
+
+            linhaScanner.close();
+
+            /*
+             * String[] valores = linha.split(",");
+             * System.out.println(Arrays.toString(valores));
+             * System.out.println(valores[3]);
+             */
+
         }
 
         scanner.close();
