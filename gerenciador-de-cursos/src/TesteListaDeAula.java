0b11dc4f21cd19b1f45a84a16b895f8cbd134f7a
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-03-02 11:19:56 -0300

    02. Listas de objetos
    Java Collections: Dominando Listas, Sets e Mapas

---
+++ gerenciador-de-cursos/src/TesteListaDeAula.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TesteListaDeAula {
+
+    public static void main(String[] args) {
+        Aula aula1 = new Aula("Revisando as ArrayLists", 21);
+        Aula aula2 = new Aula("Listas de objetos", 15);
+        Aula aula3 = new Aula("Relacionamento de listas e objetos", 15);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        //System.out.println(aulas);
+        aulas.forEach(aula -> {
+            System.out.println("Aula: " + aula.getTitulo() + ", " +
+                    aula.getTempo());
+        });
+
+        Collections.sort(aulas);
+        System.out.println("------------------------");
+
+        //System.out.println(aulas);
+
+        aulas.forEach(aula -> {
+            System.out.println("Aula: " + aula.getTitulo() + ", " +
+                    aula.getTempo());
+        });
+
+    }
+
+}
