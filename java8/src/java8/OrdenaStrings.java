1e94ff97b110d9dea35fbcf881b630ff24d4ee9d
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-03-09 15:12:35 -0300

    02. Que venham os lambdas!
    Java moderno: tire proveito dos novos recursos do Java 8

--- java8/src/java8/OrdenaStrings.java
+++ java8/src/java8/OrdenaStrings.java
@@ -1,11 +1,7 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -13,22 +9,14 @@ public class OrdenaStrings {
 
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
-		palavras.add("ditora casa do codigo");
+		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 
-		Comparator<String> comparador = new ComporadorPorTamanho();
-		// Collections.sort(palavras,comparador);
-		palavras.sort(comparador);
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
 
-//		for (String s : palavras) {
-//			System.out.println(s);
-//		}
-//		
-//		palavras.forEach(a -> {System.out.println("Palavra: " + a);});
-		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		palavras.forEach(a -> System.out.println("Palavra: " + a));
 
 	}
 
