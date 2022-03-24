5c8e82d52ce697ca1dbaa042c708732d210cb2d2
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-03-24 15:22:13 -0300

    05. Mais Streams, Collectors e APIs
    Java moderno: tire proveito dos novos recursos do Java 8

--- java8/src/java8/ExemploCursos.java
+++ java8/src/java8/ExemploCursos.java
@@ -3,6 +3,8 @@ package java8;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 class Curso {
 
@@ -37,24 +39,48 @@ public class ExemploCursos {
 
 		// Aqui esta sendo utilizado o filter para filtrar cursos que possui um
 		// detrminado numero de alunos.
-		cursos.stream().filter(c -> c.getAlunos() >= 113)
-				.forEach(c -> System.out.println("Nome cursos: " + c.getNome()));
-		;
+		// cursos.stream().filter(c -> c.getAlunos() >= 113)
+		// .forEach(c -> System.out.println("Nome cursos: " + c.getNome()));
+		// ;
 
 		// Aqui esta sendo mostrado o numero d alunos dos cursos que foram filtrados,
 		// utilizando o metodo "map" para isso
-		cursos.stream().filter(c -> c.getAlunos() >= 113).map(c -> c.getAlunos())
-				.forEach(total -> System.out.println("Quantidade alunos: " + total));
+		// cursos.stream().filter(c -> c.getAlunos() >= 113).map(c -> c.getAlunos())
+		// .forEach(total -> System.out.println("Quantidade alunos: " + total));
 		;
 
 		// Utilizando o mapToInt e possiveis trabalhar como funçoes como o sum() que
 		// soma os valores
-		int sum = cursos.stream().filter(c -> c.getAlunos() >= 113).mapToInt(Curso::getAlunos).sum();
+		// int sum = cursos.stream().filter(c -> c.getAlunos() >=
+		// 113).mapToInt(Curso::getAlunos).sum();
 
-		System.out.println("Somatoria de alunos: " + sum);
+		// System.out.println("Somatoria de alunos: " + sum);
+
+		// Filtrando cursos com mais de 50 alunos
+		// cursos.stream().filter(c -> c.getAlunos() > 50).forEach(total ->
+		// System.out.println(total.getNome()));
+
+		// cursos.stream().filter(c -> c.getAlunos() >= 100).findFirst().ifPresent(c ->
+		// System.out.println(c.getNome()));
+
+		cursos.forEach(c -> System.out.println("Antes: " + c.getNome()));
+
+		cursos.stream().filter(c -> c.getAlunos() >= 100)
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getAlunos()))
+				.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
+
+		cursos = cursos.stream().filter(c -> c.getAlunos() >= 100).collect(Collectors.toList());
+
+		cursos.forEach(c -> System.out.println("Depois: " + c.getNome()));
 		
-		//Filtrando cursos com mais de 50 alunos
-		cursos.stream().filter(c -> c.getAlunos() > 50).forEach(total -> System.out.println(total.getNome()));
+		
+		OptionalDouble average = cursos.stream().mapToInt(c -> c.getAlunos()).average();
+		
+		System.out.println(average.getAsDouble());
+		
+		List<Curso> cursosFiltrados = cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
+		
+		cursosFiltrados.forEach(c -> System.out.println(c.getNome()));
 
 	}
 
