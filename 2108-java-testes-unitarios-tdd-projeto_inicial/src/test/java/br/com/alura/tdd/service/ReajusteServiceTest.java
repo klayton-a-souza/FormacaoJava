3af1e7cad3026c2822767bc97809e3c2e4d467e6
Klayton <77679814+klayton-a-souza@users.noreply.github.com>
2022-03-26 10:16:13 -0300

    05. Mais recursos
    TDD e Java: testes automatizados com JUnit

--- 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,42 +5,64 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
+
+	private ReajusteService service;
+	private Funcionario funcionario;
+
+	// Nessa aula foi apresentado 4 anotações que normalmente e usado,
+	// 01) @BeforeEach: Essa anotação faz com que o método listado com ela seja inicializado pelo JUnit sempre que um novo teste começar.
+	// 02) @AfterEach:  Essa anotação faz com que o método listado com ela seja inicializado pelo JUnit sempre que um novo teste terminar.
+	// 03) @BeforeAll:  Essa anotação faz com que o método listado seja inicializado uma unica vez antes de começar os testes, detalhe que esse método precisa ser static.
+	// 04) @AfterAll:   Essa anotação faz com que o método listado seja inicializado uma unica vez depois de finalizar os testes, detalhe que esse método precisa ser static.
+
+	@BeforeEach
+	public void inicializar() {
+		System.out.println("Inicializando");
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("klayton", LocalDate.now(), new BigDecimal("1000.00"));
+	}
 	
+	@AfterEach
+	public void finalizar() {
+		System.out.println("fim");
+	}
+	
+	@BeforeAll
+	public static void antesDeTodos() {
+		System.out.println("ANTES DE TODOS");
+	}
+	
+	@AfterAll
+	public static void depoisDeTodos() {
+		System.out.println("DEPOIS DE TODOS");
+	}
+
 	@Test
 	public void reajustDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("klayton", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
-	
+
 	@Test
 	public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("klayton", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
-	
+
 	@Test
 	public void reajustDeveriaSerDeTresPorCentoQuandoDesempenhoForOtimo() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("klayton", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
-	
 
 }
